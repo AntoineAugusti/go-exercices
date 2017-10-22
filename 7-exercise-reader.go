f28113ebe9f41e12a6ea80da08dbc6d484f1763a
horsten <thomas@horsten.com>
2017-10-22 13:32:33 +0200

    Improve complexity for 7-exercise-reader.go (#1)
    * Update 7-exercise-reader.go
    
    That is very inefficient since it requires a function call for each byte read. Instead fill the passed slice to capacity with A's as in my solution.
    
    * Update function parameter

--- 7-exercise-reader.go
+++ 7-exercise-reader.go
@@ -5,12 +5,12 @@ import "github.com/dupoxy/go-tour-fr/reader"
 type MyReader struct{}
 
 // TODO: Add a Read([]byte) (int, error) method to MyReader.
-func (r MyReader) Read(b []byte) (n int, err error) {
-	// Infinite flux of A
-	b[0] = 'A'
-
-	// Number of bits read, error
-	return 1, nil
+func (r MyReader) Read(s []byte) (n int, err error) {
+    s = s[:cap(s)];
+    for i := range s {
+        s[i] = 'A'
+    }
+    return cap(s), nil
 }
 
 func main() {
